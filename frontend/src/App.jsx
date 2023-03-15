bba166ec1c660c7eae889808a4b727be9a17c04b
GessyBoy <ydeletrez@gmail.com>
2023-03-15 22:33:53 +0100

    featurekiller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Questions from "./pages/Questions";
 import Account from "./pages/Account";
+import Form1 from "./pages/Form1";
 
 export default function App() {
 	return (
@@ -11,6 +12,7 @@ export default function App() {
 			<Navbar />
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/formulaire' element={<Form1 />} />
 				<Route path='/faq' element={<Questions />} />
 				<Route path='/account' element={<Account />} />
 			</Routes>
---
+++ frontend/src/pages/Form1.jsx
@@ -0,0 +1,29 @@
+import React from "react"; 
+
+export default function Form1() {
+  return (
+    <>
+      <div>
+        <form className="Formulaire">
+          <input type="text" placeholder="Marque" id="text"/><br />
+          <input type="text" placeholder="Model" id="text"/><br />
+          <input type="color" id="Color" />Couleur<br />
+          <input type="number" required min="8" max="14" placeholder=" version Android min 8" id="number"/><br />
+          <input type="text" placeholder="Ram" id="text" /><br />
+          <input type="text" placeholder="Mémoire" id="text" /><br />
+          <input type="number" placeholder="Ecran min 4" step="0.1" id="number" name="inputNumber" required min="4" max="7" />"<br />
+          <input type="checkbox" id="checkbox" />Chargeur et cable<br />
+        </form>
+      </div>
+      <div>
+        <iframe
+          title="Geekbench"
+          src="https://www.kimovil.com/fr/meilleurs-mobiles-antutu"
+          sandbox="allow-same-origin allow-scripts"
+          width="100%"
+          height="500px"
+        />
+      </div>
+    </>
+  );
+}
