5adc239065026120d35d6f34a6f379df75639e5c
GessyBoy <ydeletrez@gmail.com>
2023-03-16 11:31:15 +0100

    Formulaire1et redirection état

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Questions from "./pages/Questions";
 import Account from "./pages/Account";
 import Profile from "./pages/Profile";
 import Form1 from "./pages/Form1";
+import Form2 from "./pages/Form2";
+import Reconditionnement from "./pages/Reconditionnement";
+import Recyclage from "./pages/Recyclage";
 
 export default function App() {
 	return (
@@ -14,6 +17,9 @@ export default function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/formulaire' element={<Form1 />} />
+				<Route path='/formulaire2' element={<Form2 />} />
+				<Route path='/reparation' element={<Reconditionnement />} />
+				<Route path='/recyclage' element={<Recyclage />} />
 				<Route path='/faq' element={<Questions />} />
 				<Route path='/account' element={<Account />} />
 				<Route path='/profile' element={<Profile />} />
