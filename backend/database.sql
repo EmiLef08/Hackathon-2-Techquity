dcffbfb6bf36a15da6ac8f6f138b9bc524960a4a
antoine <debordantoine0@gmail.com>
2023-03-16 11:04:08 +0100

    🤡 bdd is okkkkkkk

--- backend/database.sql
+++ backend/database.sql
@@ -1,40 +1,32 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
-CREATE SCHEMA IF NOT EXISTS `techquity` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `techquity` ;
-
-
-CREATE TABLE IF NOT EXISTS `techquity`.`phone` (
-  `phone_id` int NOT NULL AUTO_INCREMENT,
-  `brand_id` int NOT NULL,
-  PRIMARY KEY (`phone_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS `techquity`.`brand` (
-	id INT auto_increment NOT NULL,
-	brand varchar(100) NOT NULL,
-    brand_id int NOT NULL,
-	model_id INT NOT NULL,
-	CONSTRAINT brand_PK PRIMARY KEY (id),
-	CONSTRAINT brand_FK FOREIGN KEY (id) REFERENCES techquity.model(id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
-
+CREATE SCHEMA IF NOT EXISTS `techquity` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `techquity`;
 
 CREATE TABLE IF NOT EXISTS `techquity`.`model` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `brand_id` int NOT NULL,
+  `brand_id`int NOT NULL,
   `model_name` varchar(100) NOT NULL,
-  `os` int NOT NULL,
+  `os` varchar(100) NOT NULL,
   `ram` int NOT NULL,
   `memory` int NOT NULL,
   `screen_size` decimal(10,0) NOT NULL,
   `network` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `url`varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `image` varchar(255) NOT NULL,
+  CONSTRAINT `model_PK` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE IF NOT EXISTS `techquity`.`brand` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(100) NOT NULL,
+  CONSTRAINT `brand_PK` PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `techquity`.`phone` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand_id` int NOT NULL,
+  `model_id` int NOT NULL,
+ CONSTRAINT `phone_PK` PRIMARY KEY (`id`),
+CONSTRAINT `brand_FK` FOREIGN KEY (`brand_id`) REFERENCES `techquity`.`brand`(`id`),
+CONSTRAINT `model_FK` FOREIGN KEY (`model_id`) REFERENCES `techquity`.`model`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
